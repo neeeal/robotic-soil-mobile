4bd88ad0f744e261748ab19ebff4dcd3f1cf06b4
Niil <91337648+neeeal@users.noreply.github.com>
2024-04-12 19:26:31 +0800

    feat: working basic bottom sheet and drawer

--- app/index.js
+++ app/index.js
@@ -2,7 +2,15 @@ import React, { useCallback, useMemo, useRef } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { Link } from 'expo-router';
 // import BottomSheet, { BottomSheetView } from '@gorhom/bottom-sheet';
+import BottomSheetComponent from '../components/modal/bottomSheet.js';
 
 export default function Root() {
-  return <Link className=" font-normal text-lg border border-red-500" href="home">Navigate to nested route</Link>;
+  return (
+    <>
+      <Text>123</Text>
+      {/* // <Text>Here</Text> */}
+      <BottomSheetComponent/>
+      {/* // <Link className=" font-normal text-lg border border-red-500" href="home">Navigate to nested route</Link> */}
+    </>
+  )
 }
